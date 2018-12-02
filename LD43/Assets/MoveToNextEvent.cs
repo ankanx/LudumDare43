72e6b4c2e5e9ae417e4aa0429bafad0d9a7a1c77
Andreas Fransson <Ankanx@gmail.com>
2018-12-02 13:16:08 +0100

    Fixed bugs and added progression

---
+++ LD43/Assets/MoveToNextEvent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToNextEvent : MonoBehaviour {
+
+    public GameObject handler;
+
+    private void Start()
+    {
+        handler = GameObject.FindGameObjectWithTag("GameController");
+    }
+
+    public void MoveToNextEventTrigger()
+    {
+        handler.GetComponent<GameController>().GenerateEvent();
+    }
+}
